9279d95a2ef18ccb9c9de86fef732283158d45ff
duartemelo <43653646+duartemelo@users.noreply.github.com>
2023-02-15 11:38:27 +0000

    Lesson 64 is done

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,46 +1,31 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
-import ExpensesFilter from './ExpensesFilter';
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
   const [enteredYear, setEnteredYear] = useState("2020");
 
-  const onYearChange = year => {
+  const onYearChange = (year) => {
     setEnteredYear(year);
     console.log(year);
-  }
-
+  };
 
   return (
     <div>
       <Card className="expenses">
-      <ExpensesFilter onYearChange={onYearChange} year={enteredYear}/>
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
-    </Card>
+        <ExpensesFilter onYearChange={onYearChange} year={enteredYear} />
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
     </div>
-    
   );
-}
+};
 
 export default Expenses;
