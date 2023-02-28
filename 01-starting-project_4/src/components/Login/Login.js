733813afc5cd8aeb26b1cee7bcb44f57f74cfeac
duartemelo <43653646+duartemelo@users.noreply.github.com>
2023-02-28 18:30:38 +0000

    Lesson 129 done

--- 01-starting-project_4/src/components/Login/Login.js
+++ 01-starting-project_4/src/components/Login/Login.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useReducer, useContext } from "react";
+import React, {
+  useState,
+  useEffect,
+  useReducer,
+  useContext,
+  useRef,
+} from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
@@ -40,6 +46,9 @@ const Login = () => {
   });
 
   const authCtx = useContext(AuthContext);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   const { isValid: emailIsValid } = emailState; // object destructuring com alias
   const { isValid: passwordIsValid } = passwordState;
@@ -83,13 +92,21 @@ const Login = () => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    } else {
+      // password is invalid
+      passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           isValid={emailIsValid}
           type="email"
           id="email"
@@ -100,6 +117,7 @@ const Login = () => {
         />
 
         <Input
+          ref={passwordInputRef}
           isValid={passwordIsValid}
           type="password"
           id="password"
@@ -109,7 +127,7 @@ const Login = () => {
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
