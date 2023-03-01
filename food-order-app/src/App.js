c225be6cedddaf57a0370eff0205d05e85a80ea9
duartemelo <43653646+duartemelo@users.noreply.github.com>
2023-03-01 00:37:20 +0000

    Currently in lesson 133

---
+++ food-order-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Layout/Header';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header/>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './HeaderCartButton.module.css'
+
+const HeaderCartButton = () => {
+  return(
+    <button className={classes.button}>Your cart</button>
+  )
+}
+
+export default HeaderCartButton;
