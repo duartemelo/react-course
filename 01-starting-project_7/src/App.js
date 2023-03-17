82221ca1d63f1ca979f8e44175f00ecc5238b3a1
duartemelo <duarteribeirodemelo@hotmail.com>
2023-03-17 15:30:35 +0000

    Lesson 184

--- 01-starting-project_7/src/App.js
+++ 01-starting-project_7/src/App.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from 'react';
 
-import MoviesList from "./components/MoviesList";
-import "./App.css";
+import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
+import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -12,55 +13,82 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
-
-      if (!response.ok){
+      const response = await fetch('https://react-http-de3a1-default-rtdb.europe-west1.firebasedatabase.app/movies.json');
+      if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movie) => {
-        return {
-          id: movie.episode_id,
-          title: movie.title,
-          releaseDate: movie.release_date,
-          openingText: movie.opening_crawl,
-        };
-      });
-      setMovies(transformedMovies);
-    } catch (error){
+      const loadedMovies = Object.entries(data).map(([key, value]) => ({
+        id: key, ...value
+      }))
+
+      // for (const key in data){
+      //   loadedMovies.push({
+      //     id: key,
+      //     title: data[key].title,
+      //     openingText: data[key].openingText,
+      //     releaseDate: data[key].releaseDate
+      //   })
+      // }
+
+      setMovies(loadedMovies);
+      fetchMoviesHandler();
+    } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
-  }, [])
+  }, []);
 
   useEffect(() => {
     fetchMoviesHandler();
-  }, [fetchMoviesHandler])
+  }, [fetchMoviesHandler]);
 
-  let content = <p>Found no movies.</p>
-
-  if (movies.length > 0){
-    content = <MoviesList movies={movies} />
+  async function addMovieHandler(movie) {
+    const response = await fetch('https://react-http-de3a1-default-rtdb.europe-west1.firebasedatabase.app/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data);
   }
 
-  if (error){
-    content = <p>{error}</p>
+  const deleteMovieHandler = async (id) => {
+    const response = await fetch(`https://react-http-de3a1-default-rtdb.europe-west1.firebasedatabase.app/movies/${id}.json` ,{
+      method: 'DELETE'
+    })
+    if (!response.ok){
+      console.log("error")
+    }
   }
 
-  if (isLoading){
-    content = <p>Loading....</p>
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} deleteFunction={deleteMovieHandler} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        {content}
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
