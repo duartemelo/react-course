c7cbd4cad0440fd47451e327b5b73e0228737b7e
Duarte Melo <duarteribeirodemelo@hotmail.com>
2023-04-25 19:42:42 +0100

    Fetching data from Firebase (Section 17) by me

--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,53 +1,85 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
+import useHttp from "../../hooks/use-http";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+// const DUMMY_MEALS = [
+//   {
+//     id: "m1",
+//     name: "Sushi",
+//     description: "Finest fish and veggies",
+//     price: 22.99,
+//   },
+//   {
+//     id: "m2",
+//     name: "Schnitzel",
+//     description: "A german specialty!",
+//     price: 16.5,
+//   },
+//   {
+//     id: "m3",
+//     name: "Barbecue Burger",
+//     description: "American, raw, meaty",
+//     price: 12.99,
+//   },
+//   {
+//     id: "m4",
+//     name: "Green Bowl",
+//     description: "Healthy...and green...",
+//     price: 18.99,
+//   },
+// ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => {
-    return (
-      <MealItem
-        id={meal.id}
-        key={meal.id}
-        name={meal.name}
-        description={meal.description}
-        price={meal.price}
-      />
+  const [meals, setMeals] = useState([]);
+  const { isLoading, error, sendRequest } = useHttp();
+
+  const applyData = (data) => {
+    for (const [key, value] of Object.entries(data)) {
+      setMeals((prevMeals) => [
+        ...prevMeals,
+        {
+          id: key,
+          ...value,
+        },
+      ]);
+    }
+  };
+
+  useEffect(() => {
+    sendRequest(
+      {
+        url: "https://react-http-de3a1-default-rtdb.europe-west1.firebasedatabase.app/meals.json",
+      },
+      applyData
     );
-  });
+  }, [sendRequest]);
+
+  let content = <p>Loading...</p>;
+
+  if (!isLoading) {
+    if (error) {
+      content = <p>{error}</p>;
+    } else {
+      const mealsList = meals.map((meal) => {
+        return (
+          <MealItem
+            id={meal.id}
+            key={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+          />
+        );
+      });
+      content = <ul>{mealsList}</ul>;
+    }
+  }
 
   return (
     <section className={classes.meals}>
-      <Card>
-        <ul>{mealsList}</ul>
-      </Card>
+      <Card>{content}</Card>
     </section>
   );
 };
