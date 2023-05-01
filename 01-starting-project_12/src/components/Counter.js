a3398add56722388f2a699d0410101db317564fd
Duarte Melo <duarteribeirodemelo@hotmail.com>
2023-05-01 15:51:56 +0100

    Lesson 242

--- 01-starting-project_12/src/components/Counter.js
+++ 01-starting-project_12/src/components/Counter.js
@@ -4,8 +4,11 @@ import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const shown = useSelector((state) => state.shown);
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   const incrementClickHandler = () => {
     dispatch({ type: "increment" });
@@ -22,7 +25,7 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {shown && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementClickHandler}>Increment</button>
         <button onClick={increaseByFiveClickHandler}>Increase by 5</button>
--- 01-starting-project_12/src/store/index.js
+++ 01-starting-project_12/src/store/index.js
@@ -1,24 +1,56 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
-const storeReducer = (
-  state = {
-    counter: 0,
-  },
-  action
-) => {
+const initialState = {
+  counter: 0,
+  shown: true,
+};
+
+createSlice(
+  {
+    name: 'counter',
+    initialState, // same as initialState: initialState
+    reducers: {
+      increment(state) { // here we can mutate state! but not in "normal" redux, redux toolkit creates a new state under the hood
+        state.counter++;
+      },
+      decrement(state) {
+        state.counter--;
+      },
+      increase(state, action) {
+        state.counter += action.value;
+      },
+      toggleCounter(state) {
+        state.shown = !state.shown;
+      }
+    }
+  }
+);
+
+const storeReducer = (state = initialState, action) => {
+  // NEVER MUTATE STATE! RETURN A NEW ONE
   if (action.type === "increment") {
     return {
+      ...state,
       counter: state.counter + 1,
     };
   }
   if (action.type === "decrement") {
     return {
+      ...state,
       counter: state.counter - 1,
     };
   }
   if (action.type === "increase") {
     return {
+      ...state,
       counter: state.counter + action.value,
+    };
+  }
+  if (action.type === "toggle") {
+    return {
+      ...state,
+      shown: !state.shown,
     };
   }
 
