c7f34ef99008f08c246831e66faad4ae31b00a9d
Duarte Melo <duarteribeirodemelo@hotmail.com>
2023-05-10 21:22:47 +0100

    Lesson 260

--- 01-starting-project_13/src/store/cart-slice.js
+++ 01-starting-project_13/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const initialCartState = {
   show: false,
@@ -64,6 +65,49 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending card data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-http-de3a1-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        { method: "PUT", body: JSON.stringify(cart) }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+    };
+
+    sendRequest()
+      .then(() => {
+        dispatch(
+          uiActions.showNotification({
+            status: "success",
+            title: "Success!",
+            message: "Sent cart data successfully!",
+          })
+        );
+      })
+      .catch(() => {
+        dispatch(
+          uiActions.showNotification({
+            status: "error",
+            title: "Error!",
+            message: "Sending cart data failed!",
+          })
+        );
+      });
+  };
+};
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
